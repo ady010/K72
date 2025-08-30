f1a03784cfcbc2e5d98b30d09845dbe975fea5f2
Aditya Pahane <adityapahane@gmail.com>
2025-08-30 19:01:56 +0530

    Starting React

---
+++ k72/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './Pages/Home'
+import Projects from './Pages/Projects'
+import Agency from './Pages/Agency'
+
+const App = () => {
+  return (
+    <div className='h-screen w-screen bg-black text-white'>
+     
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/agency' element={<Agency />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
