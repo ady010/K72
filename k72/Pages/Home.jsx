545c3c4601e6f5971e577f7fa761bd91c174db37
Aditya Pahane <adityapahane@gmail.com>
2025-08-30 19:05:20 +0530

    Pages Folder

---
+++ k72/Pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Video from '../Components/Home/Video'
+import Herotext from '../Components/Home/Herotext'
+import Bottomtext from '../Components/Home/Bottomtext'
+
+const Home = () => {
+  return (
+    <div>
+        <div className='VIDEO h-screen w-screen fixed'>
+        <Video />
+    </div>
+    <div className='TEXTS h-screen w-screen relative flex flex-col justify-between'>
+        <Herotext />
+        <Bottomtext />
+    </div>
+    </div>
+  )
+}
+
+export default Home
